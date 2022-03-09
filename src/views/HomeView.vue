96488fcc424ada3c64eb4ee3cdb197389843ba8d
Butrahandisya <57083494+Andyoshino@users.noreply.github.com>
2022-03-09 19:47:55 +0700

    did basic-layouting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mx-auto border border-black my-10 flex h-[800px] w-[960px]">
+
+      <div class="w-1/3">
+          <div class="h-2/5 rounded-2xl overflow-hidden shadow-xl bg-slate-50">
+              <div class="bg-gradient-to-b from-red-500 to-pink-500 text-white h-8 py-1 text-center">
+                  <h1>Input Task</h1>
+              </div>
+
+              <div class="w-full p-6">
+
+
+
+              </div>
+
+
+          </div>
+
+          <div class="h-3/5 rounded-2xl overflow-hidden shadow-xl bg-slate-50 mt-2">
+              <div class="bg-gradient-to-b from-blue-500 to-sky-500 text-white h-8 py-1 text-center">
+                  <h1>Query Setting</h1>
+              </div>
+          </div>
+
+      </div>
+
+      <div class="border border-red-400 w-2/3">asd</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
